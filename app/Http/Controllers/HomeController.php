9f096c5ab1333405bf251b8540b4d9d4283093b1
Scwein <danila20102005@gmail.com>
2026-03-09 19:43:23 +0300

    add instance request

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,27 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
-{
+{   
     public function index()
     {
         return view('index', ['title_list' => 'List of tasks:', 'title' => 'Home'])->with('tasks', Task::get()->toArray());
     }
 
-    public function store()
+    public function store(Request $request)
     {
         $messages = [
-        'required' => 'Поле обязательно для заполнения',
-        'min' => 'Минимальная длина 6 символов'
+            'required' => 'Поле обязательно для заполнения',
+            'min' => 'Минимальная длина 6 символов'
         ];
 
-        request()->validate([
+        $request->validate([
             'task' => 'required|min:6'
         ], $messages);
-
+        
         $task = new Task([
-            'task_desc' => request('task'),
+            'task_desc' => $request->input('task'),
         ]);
         $task->save();
 
